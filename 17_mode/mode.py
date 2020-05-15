8b134fbf77aee313092f7d437170834ae61a64d5
kevinchheang <kevin.chh89@gmail.com>
2020-05-14 22:44:13 -0700

    Completed exercise 17_mode

--- 17_mode/mode.py
+++ 17_mode/mode.py
@@ -11,3 +11,18 @@ def mode(nums):
         >>> mode([2, 2, 3, 3, 2])
         2
     """
+    # dictionary to hold frequency values
+    freq_dict = {}
+
+    # add num to dictioinary and count frequency
+    for num in nums:
+        freq_dict[num] = freq_dict.get(num, 0) + 1
+
+    # get most frequence value (most occurence)
+    max_number = max(freq_dict.values())
+
+    # find the most frequence index
+    for (key, value) in freq_dict.items():
+        if value == max_number:
+            return key
+
