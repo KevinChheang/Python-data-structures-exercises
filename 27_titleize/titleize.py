1e15d1cd3e36f533e4d3e5ca937ae2e92403e496
kevinchheang <kevin.chh89@gmail.com>
2020-05-15 23:52:11 -0700

    Completed exercise 27_titleize

--- 27_titleize/titleize.py
+++ 27_titleize/titleize.py
@@ -7,3 +7,17 @@ def titleize(phrase):
         >>> titleize('oNLy cAPITALIZe fIRSt')
         'Only Capitalize First'
     """
+    # easy way to just use the predefined method
+    # return phrase.title()
+
+    # my own implementation of title()
+    split_phrase = phrase.lower().split(" ")
+    titleize_word = ""
+
+    for word in split_phrase:
+        if word != split_phrase[-1]:
+            titleize_word += word.capitalize() + " "
+        else:
+            titleize_word += word.capitalize()
+
+    return titleize_word
