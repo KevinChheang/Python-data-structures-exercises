2a88b3021dfcd7592e1b6029a5c7ecd2b02d7fd4
kevinchheang <kevin.chh89@gmail.com>
2020-05-19 13:45:22 -0700

    Completed exercise 29_includes

--- 29_includes/includes.py
+++ 29_includes/includes.py
@@ -30,3 +30,29 @@ def includes(collection, sought, start=None):
         >>> includes({"apple": "red", "berry": "blue"}, "blue")
         True
     """
+    # check if collectioin is of type "str", "list" or "tuple"
+    if type(collection) == str or type(collection) == list or type(collection) == tuple:
+        # if start index is provided
+        # search collection from start index
+        if start:
+            for i in range(start, len(collection)):
+                if sought == collection[i]:
+                    return True
+        # else if start index is not provided
+        # search whole collection
+        elif sought in collection:
+            return True
+    # check for type of "set"
+    # ignore start index even if given, because unorder data structure
+    elif type(collection) == set:
+        for item in collection:
+            if sought == item:
+                return True
+    # check for type of "dict"
+    # ignore start index even if given, because unorder data structure
+    elif type(collection) == dict:
+        for (k, v) in collection.items():
+            if sought == v:
+                return True
+
+    return False
